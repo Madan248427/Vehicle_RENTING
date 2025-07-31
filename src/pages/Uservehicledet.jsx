5e13fcfbae80ced03175c903a71205dd06cb2b7c
Madan248427 <katuwalmadan55@gmail.com>
2025-08-01 02:10:07 +0545

    Fix folder casing: sidebar_temp → Sidebar

--- src/pages/Uservehicledet.jsx
+++ src/pages/Uservehicledet.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from '../components/User/Slidebar/sidebar'
+import Sidebar from '../components/User/Sidebar/sidebar.jsx'
 import VehicleDetails from '../components/User/Vehicle.css/Vehicle_details'
 
 export default function Uservehicledet() {
