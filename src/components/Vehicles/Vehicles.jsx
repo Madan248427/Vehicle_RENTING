938f0a9e4696fd179441035e82ba0eb586eb1ff3
Madan248427 <katuwalmadan55@gmail.com>
2025-08-01 03:08:13 +0545

    fix: correct casing in Vehicle.module.css import path

--- src/components/Vehicles/Vehicles.jsx
+++ src/components/Vehicles/Vehicles.jsx
@@ -4,7 +4,8 @@ import { collection, getDocs } from "firebase/firestore"
 import { db } from "../firebase/firebase"
 import { Car, Star } from "lucide-react"
 import { Link } from "react-router-dom"
-import styles from "../vehicles/Vehicle.module.css";
+import styles from "./Vehicle.module.css";
+
 
 
 export default function Vehicles({ handleVehicleClick }) {
